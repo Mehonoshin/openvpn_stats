624dea82ace353ad7de3b3afce741ccbcda6528f
Stanislav Mekhonoshin <ejabberd@gmail.com>
2015-03-09 19:43:23 +0300

    Count demo stats

--- server/statistics.go
+++ server/statistics.go
@@ -2,10 +2,18 @@ package server
 
 import (
   "fmt"
+  "strconv"
   "openvpn_stats/dto"
 )
 
 type Statistics struct {
+  TotalBytesIn     int64
+  TotalBytesOut    int64
+  ClientsConnected int64
+}
+
+func (s Statistics) String() string {
+  return fmt.Sprintf("%v, %v, %v", s.TotalBytesIn, s.TotalBytesOut, s.ClientsConnected)
 }
 
 type Server struct {
@@ -15,7 +23,24 @@ type Client struct {
 }
 
 func NewStatistics(channel chan []dto.Client) {
+  totalStatistics := Statistics{0, 0, 0}
+
   for clients := range channel {
-    fmt.Println("[Stats received]", clients)
+    totalStatistics = countStatistics(totalStatistics, clients)
+    fmt.Println("Current stats is:", totalStatistics)
   }
+}
+
+func countStatistics(statistics Statistics, clients []dto.Client) Statistics {
+  var totalIn, totalOut, totalClients int64
+  totalIn, totalOut, totalClients = statistics.TotalBytesIn, statistics.TotalBytesOut, statistics.ClientsConnected
+
+  for i := 0; i < len(clients); i++ {
+    bytesIn, _ := strconv.ParseInt(clients[i].BytesReceived, 10, 0)
+    totalIn = totalIn + bytesIn
+    bytesOut, _ := strconv.ParseInt(clients[i].BytesSent, 10, 0)
+    totalOut = totalOut + bytesOut
+    totalClients++
+  }
+  return Statistics{totalIn, totalOut, totalClients}
 }
